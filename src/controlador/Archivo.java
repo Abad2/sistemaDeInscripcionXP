5585cec7fbc280e18c94b96e35c8069378165b7a
Abad Ramos Maldonado <abad.ramos@ceis.cs.umss.edu.bo>
2018-11-16 14:04:23 -0400

    inicializacion del proyencto

---
+++ src/controlador/Archivo.java
@@ -0,0 +1,40 @@
+package controlador;
+
+public class Archivo {
+	private String idArch, nombreArchivo, puestoEnVigenvia, vigenciaActual, formato, fechaCreacion;
+	
+	public Archivo(){
+		
+		
+	}
+	
+	public String getIdArch() {
+		return idArch;
+	}
+
+	public String getNombreArchivo() {
+		return nombreArchivo;
+	}
+
+	public String getPuestoEnVigenvia() {
+		return puestoEnVigenvia;
+	}
+
+	public String getVigenciaActual() {
+		return vigenciaActual;
+	}
+
+	public String getFormato() {
+		return formato;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
